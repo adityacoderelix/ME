9318714ef5a8c6f4feae87b34ea5df89a0ab093b
aditya <adityaparuleker.cr@gmail.com>
2025-08-20 18:04:48 +0530

    Added minimum amount allowed to be entered for per day property price  tag displayed

--- src/app/host/dashboard/add-listing/steps/(part-three)/step-two.jsx
+++ src/app/host/dashboard/add-listing/steps/(part-three)/step-two.jsx
@@ -12,32 +12,34 @@ export function SetPricing({ updateFormData, formData }) {
 
   return (
     <div className="space-y-8 max-w-3xl flex flex-col items-center justify-center mx-auto ">
-          <TextReveal>
-
-        
-
-      <h3 className="text-xl md:text-2xl font-bricolage text-absoluteDark font-semibold">
-        Let's set the pricing{" "}
-      </h3>
+      <TextReveal>
+        <h3 className="text-xl md:text-2xl font-bricolage text-absoluteDark font-semibold">
+          Let's set the pricing{" "}
+        </h3>
       </TextReveal>
       <TextReveal>
-      <div>
-        <h3 className="text-stone text-lg  mb-2">Set your price (per night)</h3>
-        <div className="flex items-center  space-x-2">
-          <span className="text-4xl mr-2 font-medium">₹</span>
-          <input
-            id="basePrice"
-            type="number"
-            value={basePrice}
-            onChange={(e) => {
-              setBasePrice(e.target.value);
-              handleChange(e);
-            }}
-            placeholder="Base price / night"
-            className="max-w-[300px] border border-gray-300 px-8 focus:ring-primaryGreen focus:ouline-none mt-4 rounded-md ouline-none text-xl py-4 text-absoluteDark no-spinner"
-          />
+        <div>
+          <h3 className="text-center text-stone text-lg  mb-2">
+            Set your price (per night).
+          </h3>
+          <div className="flex items-center  space-x-2">
+            <span className="text-4xl mr-2 font-medium">₹</span>
+            <input
+              id="basePrice"
+              type="number"
+              value={basePrice}
+              onChange={(e) => {
+                setBasePrice(e.target.value);
+                handleChange(e);
+              }}
+              placeholder="Base price / night"
+              className="max-w-[300px] border border-gray-300 px-8 focus:ring-primaryGreen focus:ouline-none mt-4 rounded-md ouline-none text-xl py-4 text-absoluteDark no-spinner"
+            />
+          </div>
+          <div className="text-center text-sm pt-4 text-muted-foreground">
+            Minimum amount allowed is ₹501.
+          </div>
         </div>
-      </div>
       </TextReveal>
     </div>
   );
