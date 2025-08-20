2d05df734e621b8276c60136d63b1d4b02afa008
aditya <adityaparuleker.cr@gmail.com>
2025-08-20 18:06:11 +0530

    Add pincode validation

--- src/app/host/dashboard/add-listing/steps/validate.js
+++ src/app/host/dashboard/add-listing/steps/validate.js
@@ -27,10 +27,9 @@ export const validatePlaceType = (formData) => {
 //   };
 // };
 
-
 export const validateLocationForm = (formData) => {
   const { address, validRegistrationNo } = formData;
-  
+  const re = /^[0-9\b]+$/;
   // Validate essential address fields
   const isAddressValid =
     address.latitude &&
@@ -40,16 +39,19 @@ export const validateLocationForm = (formData) => {
     address.city?.trim() !== "" &&
     address.state?.trim() !== "" &&
     address.pincode?.trim() !== "" &&
+    address.pincode?.trim().length == 6 &&
+    re.test(address.pincode?.trim()) &&
     address.country?.trim() !== "";
-  
+
   // Check that a registration number is provided in the address
   const isRegistrationNumberProvided =
     address.registrationNumber?.trim() !== "";
-  
+
   // The form is valid only if all address fields are filled,
   // a registration number is provided, and it has been verified.
-  const isValid = isAddressValid && isRegistrationNumberProvided && validRegistrationNo;
-  
+  const isValid =
+    isAddressValid && isRegistrationNumberProvided && validRegistrationNo;
+
   let errorMessage = "";
   if (!isRegistrationNumberProvided) {
     errorMessage = "Please provide your property registration number.";
@@ -58,13 +60,31 @@ export const validateLocationForm = (formData) => {
   } else if (!isAddressValid) {
     errorMessage = "Please fill in all address fields.";
   }
-  
+
   return { isValid, errorMessage };
 };
+export const validateTime = (formData) => {
+  const { checkinTime, checkoutTime } = formData;
+  const re = /^[0-9\b]+$/;
+  const isValid =
+    checkinTime?.trim() !== "" &&
+    re.test(checkinTime?.trim()) &&
+    checkinTime?.trim() > 0 &&
+    checkinTime?.trim() < 24 &&
+    checkoutTime?.trim() !== "" &&
+    re.test(checkoutTime?.trim()) &&
+    checkoutTime?.trim() > 0 &&
+    checkoutTime?.trim() < 24;
 
+  let errorMessage = "";
+  if (!isValid) {
+    errorMessage = "Please provide number.";
+  }
 
+  return { isValid, errorMessage };
+};
 export const validateDescribeYourPlace = (formData) => {
-  const {  guests, bedrooms, beds, bathrooms } = formData;
+  const { guests, bedrooms, beds, bathrooms } = formData;
 
   return {
     isValid:
@@ -75,29 +95,25 @@ export const validateDescribeYourPlace = (formData) => {
   };
 };
 
-
-
 export const validateBathroomSelector = (formData) => {
-  const totalBathrooms = formData.bathroomTypes.private + 
-                         formData.bathroomTypes.shared + 
-                         formData.bathroomTypes.dedicated;
+  const totalBathrooms =
+    formData.bathroomTypes.private +
+    formData.bathroomTypes.shared +
+    formData.bathroomTypes.dedicated;
 
   return {
-    isValid: totalBathrooms > 0,  // At least one bathroom must be selected
+    isValid: totalBathrooms > 0, // At least one bathroom must be selected
     errorMessage: "Please select at least one bathroom type.",
   };
 };
 
-
 export const validateOccupancySelector = (formData) => {
   return {
     isValid: Array.isArray(formData.occupancy) && formData.occupancy.length > 0,
-    errorMessage: "Please select at least one option for 'Who else might be there?'.",
+    errorMessage:
+      "Please select at least one option for 'Who else might be there?'.",
   };
 };
-
-
-
 
 export const validateAmenitiesSelector = (formData) => {
   return {
@@ -115,7 +131,9 @@ export const validateAddPhotos = (formData) => {
 
 export const validateListingDetails = (formData) => {
   return {
-    isValid: formData.title.trim().length >=3  && formData.description.trim().length >15,
+    isValid:
+      formData.title.trim().length >= 3 &&
+      formData.description.trim().length > 15,
     errorMessage: "",
   };
 };
